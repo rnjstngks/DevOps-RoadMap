f7419edb94b982c443ade4e14cd3a5985b8d9f4a
rnjstngks <rnjstngks267@naver.com>
2024-12-06 15:35:41 +0900

    change static file path
    change static file path

--- static-site-server/deploy.py
+++ static-site-server/deploy.py
@@ -29,10 +29,7 @@ def upload_files(ssh_client, local_dir, remote_dir):
 def configure_nginx(ssh_client, remote_dir):
     """Nginx 설정을 수정하고 서비스를 재시작합니다."""
     commands = [
-        f"sudo sed -i 's|root .*;|root {remote_dir};|g' /etc/nginx/sites-available/default",
-        "sudo sed -i 's|index .*;|index index.html;|g' /etc/nginx/sites-available/default",
-        "sudo nginx -t",  # 설정 파일 테스트
-        "sudo systemctl restart nginx"  # Nginx 서비스 재시작
+        f"systemctl restart nginx"  # Nginx 서비스 재시작
     ]
 
     try:
@@ -42,7 +39,7 @@ def configure_nginx(ssh_client, remote_dir):
             error = stderr.read().decode()
             if error:
                 print(f"명령 실행 중 오류 발생: {error}")
-        print("Nginx 설정 완료 및 서비스 재시작!")
+        print("Nginx 서비스 재시작!")
     except Exception as e:
         print(f"Nginx 설정 실패: {e}")
 
