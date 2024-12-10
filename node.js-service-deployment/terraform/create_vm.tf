76bba893c2ab9dd28dfca9a486b4e6bb8e1ac496
rnjstngks <rnjstngks267@naver.com>
2024-12-10 15:38:21 +0900

    test11

--- node.js-service-deployment/terraform/create_vm.tf
+++ node.js-service-deployment/terraform/create_vm.tf
@@ -15,36 +15,32 @@ resource "vsphere_virtual_machine" "test_vm" {
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
     customize {
-        linux_options { # Linux 운영 체제의 사용자 정의와 관련
-            host_name = "test-node"
-            domain = "smrc.klab-a"
-        }
-        network_interface {
-            ipv4_address = "10.10.91.4"
-            ipv4_netmask = 24
+      linux_options { # Linux 운영 체제의 사용자 정의와 관련
+        host_name = "test-node"
+        domain    = "smrc.klab-a"
       }
-      ipv4_gateway = "10.10.91.1"
+      network_interface {
+        ipv4_address = "10.10.91.4"
+        ipv4_netmask = 24
+      }
+      ipv4_gateway    = "10.10.91.1"
       dns_server_list = ["10.10.92.201"] # 리스트 형태로 해주어야 IP로 입력 가능
     }
   }
 
-  extra_config = {
-    
-  }
-
   provisioner "remote-exec" {
-    inline = [ 
+    inline = [
       "mkdir -p /root/.ssh",
       "echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzm6eyYXwDZR8qmjExHCwMDoeb3DVG/wbl8wqR+IklX root@S2300289' >> /root/.ssh/authorized_keys",
       "chmod 600 /root/.ssh/authorized_keys",
       "chmod 700 /root/.ssh"
-     ]
-     
-     connection {
-      type = "ssh"
-      user = "root"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "root"
       password = "1234"
-      host = self.default_ip_address
-     }
+      host     = "10.10.91.4"
+    }
   }
 }
