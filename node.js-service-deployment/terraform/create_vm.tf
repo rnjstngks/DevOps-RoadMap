d49bf6dcf26d002d9003f3b67b0464cec74ab91c
rnjstngks <rnjstngks267@naver.com>
2024-12-10 14:27:46 +0900

    add dns ip

--- node.js-service-deployment/terraform/create_vm.tf
+++ node.js-service-deployment/terraform/create_vm.tf
@@ -15,7 +15,7 @@ resource "vsphere_virtual_machine" "test_vm" {
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
     customize {
-        linux_options {
+        linux_options { # Linux 운영 체제의 사용자 정의와 관련
             host_name = "test-node"
             domain = "smrc.klab-a"
         }
@@ -24,6 +24,7 @@ resource "vsphere_virtual_machine" "test_vm" {
             ipv4_netmask = 24
       }
       ipv4_gateway = "10.10.91.1"
+      dns_server_list = "10.10.92.201"
     }
   }
 }
